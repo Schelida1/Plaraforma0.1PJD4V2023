d69563367b922cdae99c1deba8f6b293c8fc8e6d
Schelida Lamusse Silva Belchior <schelida.s@academico.ifrn.edu.br>
2023-04-03 17:50:07 -0300

    Aula03/04

--- Projeto0.1/Assets/Scripts/GameManager.cs
+++ Projeto0.1/Assets/Scripts/GameManager.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+    private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(transform);
+        }
         
+        else 
+            Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+       LoadScene("MainMenu");
     }
 
     // Update is called once per frame
-    void Update()
+    
+
+    public void LoadScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
